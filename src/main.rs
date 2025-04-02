7d807729760fd50222bf1e7f5b3ee76af6e1733a
Callum Gill <callum.gill.1993.cg@gmail.com>
2025-04-02 15:54:18 -0700

    Added the start of a webserver to get all products. Needs work

--- src/main.rs
+++ src/main.rs
@@ -14,4 +14,16 @@ async fn main() {
     seed_products(&pool).await;
 
     println!("Database seeded successfully!");
+
+    let app = Router::new()
+        .route("/products", get(get_all_products))
+        .layer(axum::extract::Extension(pool));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Server running on http://{}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }

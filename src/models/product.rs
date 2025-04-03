4a04eba56690b008af7a12c21948f7f2c16c3152
Callum Gill <callum.gill.1993.cg@gmail.com>
2025-04-03 10:06:38 -0700

    Fixed build errors

--- src/models/product.rs
+++ src/models/product.rs
@@ -2,9 +2,9 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, sqlx::FromRow)]
 pub struct Product {
-    pub id: i64,            // This represents the primary key (auto-incrementing ID in your database)
-    pub name: String,       // Name of the product
-    pub description: String, // Description of the product
-    pub price: f64,         // Price of the product
-    pub stock: i32,         // Stock count
+    pub id: i64,
+    pub name:  Option<String>,
+    pub description:  Option<String>,
+    pub price: f64,
+    pub stock: i64,
 }

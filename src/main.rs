2bef428d8c471a98fde96a7bcf76f27f03abf6fe
Callum Gill <callum.gill.1993.cg@gmail.com>
2025-04-08 10:15:44 -0700

    Refactored main to only seed database if the seed args is provided

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod controllers;
 mod routes;
 mod models;
 
+use std::env;
 use crate::database::connect;
 use crate::seeders::seed_products;
 use crate::{routes::create_routes};
@@ -11,15 +12,19 @@ use crate::{routes::create_routes};
 #[tokio::main]
 async fn main() {
 
+    let args: Vec<String> = env::args().collect();
     let pool = connect().await;
 
     println!("Connected to the database successfully!");
 
-    seed_products(&pool).await;
-
-    println!("Database seeded successfully!");
+    if args.len() > 1 && args[1] == "seed" {
+        seed_products(&pool).await;
+        println!("Database seeded successfully!");
+    }
 
     let app = create_routes(pool);
+
+    println!("Server running on http://127.0.0.1:3000");
 
     axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
         .serve(app.into_make_service())
